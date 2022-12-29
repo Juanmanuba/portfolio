f23e1b5e3cbcd04f73513fc6a3d97d9706b5480b
Juanmanuba <jmnbvm@gmail.com>
2022-12-28 20:09:31 -0400

    set up

---
+++ src/assets/properties.images.js
@@ -0,0 +1,15 @@
+import rewardMansion from './images/mansion-400.f285d81b.png'
+import house from './images/house.png'
+import mansion from './images/mansion.png'
+import building from './images/building.png'
+import skyscraper from './images/skyscraper.png'
+import logo from './images/logo.png'
+
+export const propertyImages = {
+    rewardMansion,
+    house,
+    mansion,
+    building,    
+    skyscraper,
+    logo
+}
---
+++ src/components/hero/hero.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './hero.component.module.css';
+
+export default function Hero() {
+  return (
+    <section className={styles.hero}>
+      <h1>Hero</h1>
+    </section>
+  );
+}
