a31609fc8eb794a0e9cae87f474e5c37e39ac582
Juanmanuba <jmnbvm@gmail.com>
2023-01-03 03:37:57 -0400

    aaa

---
+++ src/components/services/services-info.js
@@ -0,0 +1,67 @@
+const servicesInfo = [
+  {
+    title: 'Landing page design',
+    icon: 'bx-credit-card-front',
+    bulletsText: [
+      {
+        text: 'I develop the user interface',
+      },
+      {
+        text: 'Web page development',
+      },
+      {
+        text: 'I create UX element interactions',
+      },
+      {
+        text: 'I position your company brand',
+      },
+      {
+        text: 'Design and mockups of products for companies',
+      },
+    ],
+  },
+  {
+    title: 'Data analysis for companies',
+    icon: 'bx-credit-card-front',
+    bulletsText: [
+      {
+        text: 'I develop the user interface',
+      },
+      {
+        text: 'Web page development',
+      },
+      {
+        text: 'I create UX element interactions',
+      },
+      {
+        text: 'I position your company brand',
+      },
+      {
+        text: 'Design and mockups of products for companies',
+      },
+    ],
+  },
+  {
+    title: 'Machine Learning strategies',
+    icon: 'bx-credit-card-front',
+    bulletsText: [
+      {
+        text: 'I develop the user interface',
+      },
+      {
+        text: 'Web page development',
+      },
+      {
+        text: 'I create UX element interactions',
+      },
+      {
+        text: 'I position your company brand',
+      },
+      {
+        text: 'Design and mockups of products for companies',
+      },
+    ],
+  },
+];
+
+export default servicesInfo;
--- src/components/services/services.component.js
+++ src/components/services/services.component.js
@@ -4,76 +4,11 @@ import Title from '../title/title.component';
 import Paragraph from '../paragraph/paragraph.component';
 import ServiceBox from '../service-box/service-box.component';
 import ServiceModal from '../service-modal/service-modal.component';
+import servicesInfo from './services-info';
 
 export default function Services() {
   const [showServiceModal, setShowServiceModal] = useState(false);
   const [chooseServiceModal, setChooseServiceModal] = useState(0);
-
-  const servicesInfo = [
-    {
-      title: 'Landing page design',
-      icon: 'bx-credit-card-front',
-      bulletsText: [
-        {
-          text: 'I develop the user interface',
-        },
-        {
-          text: 'Web page development',
-        },
-        {
-          text: 'I create UX element interactions',
-        },
-        {
-          text: 'I position your company brand',
-        },
-        {
-          text: 'Design and mockups of products for companies',
-        },
-      ],
-    },
-    {
-      title: 'Data analysis for companies',
-      icon: 'bx-credit-card-front',
-      bulletsText: [
-        {
-          text: 'I develop the user interface',
-        },
-        {
-          text: 'Web page development',
-        },
-        {
-          text: 'I create UX element interactions',
-        },
-        {
-          text: 'I position your company brand',
-        },
-        {
-          text: 'Design and mockups of products for companies',
-        },
-      ],
-    },
-    {
-      title: 'Machine Learning strategies',
-      icon: 'bx-credit-card-front',
-      bulletsText: [
-        {
-          text: 'I develop the user interface',
-        },
-        {
-          text: 'Web page development',
-        },
-        {
-          text: 'I create UX element interactions',
-        },
-        {
-          text: 'I position your company brand',
-        },
-        {
-          text: 'Design and mockups of products for companies',
-        },
-      ],
-    },
-  ];
 
   const services = servicesInfo.map((item, index) => {
     return (
