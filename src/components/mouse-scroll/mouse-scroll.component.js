1c1ef7f526a9b7342303cf211e41a7e1d114434f
Juanmanuba <jmnbvm@gmail.com>
2023-01-09 10:19:32 -0400

    last details

--- src/components/mouse-scroll/mouse-scroll.component.js
+++ src/components/mouse-scroll/mouse-scroll.component.js
@@ -2,15 +2,17 @@ import React from 'react';
 import styles from './mouse-scroll.component.module.css';
 export default function MouseScroll() {
   return (
-    <div className={styles.mouse_scroll}>
-      <div className={styles.mouse}>
-        <div className={styles.wheel}></div>
+    <a href="#about">
+      <div className={styles.mouse_scroll}>
+        <div className={styles.mouse}>
+          <div className={styles.wheel}></div>
+        </div>
+        <div>
+          <span className={`${styles.m_scroll_arrows} ${styles.unu}`}></span>
+          <span className={`${styles.m_scroll_arrows} ${styles.doi}`}></span>
+          <span className={`${styles.m_scroll_arrows} ${styles.trei}`}></span>
+        </div>
       </div>
-      <div>
-        <span className={`${styles.m_scroll_arrows} ${styles.unu}`}></span>
-        <span className={`${styles.m_scroll_arrows} ${styles.doi}`}></span>
-        <span className={`${styles.m_scroll_arrows} ${styles.trei}`}></span>
-      </div>
-    </div>
+    </a>
   );
 }
